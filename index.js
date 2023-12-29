9f1eb9892e9a9a8a36fcf3ff36d389cf0e2e6984
mzrv <vikask0101@outlook.com>
2023-12-29 08:56:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send("twitter page")
+})
+
+app.get("/other", (req, res) => {
+    res.send("<h2>nice<h2/>")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('App is listening on port ' + process.env.PORT)
+})
